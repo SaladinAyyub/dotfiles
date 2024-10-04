e837392f3227f24a1fe92555ff2409035eab630a
Saladin <saladin@tuta.com>
2024-10-04 07:28:56 +0530

    Switch to pyright

--- .config/nvim/lua/configs/lspconfig.lua
+++ .config/nvim/lua/configs/lspconfig.lua
@@ -2,7 +2,7 @@
 require("nvchad.configs.lspconfig").defaults()
 
 local lspconfig = require "lspconfig"
-local servers = { "html", "cssls", "htmx", "templ", "basedpyright" }
+local servers = { "html", "cssls", "htmx", "templ", "pyright" }
 local util = require "lspconfig/util"
 local nvlsp = require "nvchad.configs.lspconfig"
 
