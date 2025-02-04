b191c3d61fb3fc5f8d87d8f3d24f9e209969d030
Saladin <saladin@tuta.com>
2025-02-05 02:32:39 +0530

    switch/config to ghostty

--- nix/home/ghostty.nix
+++ nix/home/ghostty.nix
@@ -4,4 +4,17 @@
 }:
 {
   enable = true;
+  enableZshIntegration = true;
+  settings = {
+    theme = "tokyonight";
+    font-size = 14;
+    window-decoration = false;
+    gtk-titlebar = false;
+    window-width = 140;
+    window-height = 40;
+    window-padding-x = 5;
+    window-padding-y = 5;
+    window-save-state = "always";
+    background-opacity = 0.9;
+  };
 }
