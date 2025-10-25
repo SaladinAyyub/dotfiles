ca5a8cfc32f2a4fd8f0f61293727f2d8b9608863
Saladin <dev@saladin.pro>
2025-10-26 02:39:56 +0530

    Make alacritty the default, increase font size

--- nix/home/alacritty.nix
+++ nix/home/alacritty.nix
@@ -12,16 +12,12 @@
         x = 4;
         y = 8;
       };
-      decorations = "none";
+      decorations = "full";
       opacity = 0.8;
       startup_mode = "Windowed";
       title = "Alacritty";
       dynamic_title = true;
       decorations_theme_variant = "None";
-      dimensions = {
-        columns = 140;
-        lines = 40;
-      };
     };
 
     general = {
@@ -40,7 +36,7 @@
         };
       in
       {
-        size = 14;
+        size = 15;
         normal = jetbrainsMono "Regular";
         bold = jetbrainsMono "Bold";
         italic = jetbrainsMono "Italic";
