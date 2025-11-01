239588ad13def7ccd79771341c187a5f51f5767f
Saladin <dev@saladin.pro>
2025-11-01 07:00:06 +0530

    flake update

--- nix/home/home.nix
+++ nix/home/home.nix
@@ -23,7 +23,7 @@
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = with pkgs; [
-    protonup
+    protonup-ng
     bibata-cursors
     morewaita-icon-theme
     inputs.zen-browser.packages.x86_64-linux.default
